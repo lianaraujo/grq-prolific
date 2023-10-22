35890fa464bfa1d1e6ac9f6c5886e0c1cd1173be
Lian <liansutil@gmail.com>
2023-10-22 20:13:33 -0300

    adding url

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject grq-prolific "0.1.0-SNAPSHOT"
   :author "Lian Araujo Fernandes"
   :description "api for getting random quotes"
+  :url "https://github.com/lianaraujo/grq-prolific"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
